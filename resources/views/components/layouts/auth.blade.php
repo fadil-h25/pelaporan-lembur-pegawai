af8f1a9cadc0ee05f11f1fb5607e9e0bb77b961c
Fadil Hambali <fadilhambali25@gmail.com>
2026-04-30 15:08:07 +0800

    refactor: clean up HTML structure and formatting in auth and login views

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="min-h-screen font-sans antialiased bg-base-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
 
-  
-    <x-main>
-        <x-slot:content>
-            {{ $slot }}
-        </x-slot:content>
-    </x-main>
-  
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 
-    {{--  TOAST area --}}
-    <x-toast />
-</body>
+    <body class="min-h-screen font-sans antialiased bg-base-100">
+
+        {{ $slot }}
+
+        {{--  TOAST area --}}
+        <x-toast />
+    </body>
+
 </html>
