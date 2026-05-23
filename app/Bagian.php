154264cc10b0ccc4b9849d158f976605675978da
Fadil Hambali <fadilhambali25@gmail.com>
2026-05-23 12:32:21 +0800

    feat: add SDMKUANGAN case to Bagian enum and update label method

--- app/Bagian.php
+++ app/Bagian.php
@@ -7,6 +7,8 @@ enum Bagian: string
     case BAGIAN1 = 'bagian1';
     case BAGIAN2 = 'bagian2';
     case BAGIAN3 = 'bagian3';
+    case SDMKUANGAN= 'SDM dan Keuangan';
+  
 
     public function label(): string
     {
@@ -14,6 +16,7 @@ enum Bagian: string
             self::BAGIAN1 => 'Bagian 1',
             self::BAGIAN2 => 'Bagian 2',
             self::BAGIAN3 => 'Bagian 3',
+            self::SDMKUANGAN => 'SDM dan Keuangan',
         };
     }
 }
